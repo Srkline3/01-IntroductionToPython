98612ec31f637ac2e207989ae73618b676973936
Srkline3 <klinesr@rose-hulman.edu>
2018-11-26 17:08:47 -0500

    Good boys commit

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Steven Kline.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,26 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+turtleMom = rg.SimpleTurtle()
+turtleDad = rg.SimpleTurtle()
+
+turtleDad.pen = rg.Pen('blue', 3)
+turtleMom.pen = rg.Pen('red', 3)
+turtleMom.speed = 1000
+turtleDad.speed = 500
+bigness = 10
+sids=3
+for k in range(18):
+    turtleMom.draw_regular_polygon(6, bigness)
+    turtleMom.pen_up()
+    turtleMom.right(15)
+    turtleMom.pen_down()
+    bigness = bigness*1.75
+
+for k in range(18):
+    turtleDad.draw_regular_polygon(sids, 60)
+    sids=sids+1
+
+window.close_on_mouse_click()
